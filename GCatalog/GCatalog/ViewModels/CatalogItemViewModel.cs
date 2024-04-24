56816a8faecff5cb3f92c8f4c35ec907c0d9499a
Cristofer Binimelis <cristoferbinimelis@gmail.com>
2024-04-23 20:53:16 -0400

    We're gonna make some serious changes

--- GCatalog/GCatalog/ViewModels/CatalogItemViewModel.cs
+++ GCatalog/GCatalog/ViewModels/CatalogItemViewModel.cs
@@ -18,6 +18,7 @@ namespace GCatalog.ViewModels
         private async void LoadCatalog(string catUrl = "http://www.dalong.net/reviews/mg/mg_cata1_e.htm")
         {
             var web = new HtmlWeb();
+            web.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0";
             var gunplaDocument = web.Load(catUrl);
 
             var categoryList = new List<Category>();
